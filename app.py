9174fb902ec1f71524324ca8b50583f5a8f240d1
Philippe de Bekker <P.M.deBekker@student.tudelft.nl>
2023-05-09 00:03:28 +0200

    Add CORS to Flask

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 import os
 import lib_remla19
 
 app = Flask(__name__)
+CORS(app)
 MODEL_URL =  os.environ.get("MODEL_URL", default="http://localhost:8080/sentiment")
 
 @app.route("/")
