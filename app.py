2b0f421af01dff3cc3771afca7e5c63b31663ad9
Luuk van Campen <luuk.vanCampen@student.ru.nl>
2023-05-16 15:21:22 +0200

    added metric for counting page visits

--- app.py
+++ app.py
@@ -8,8 +8,12 @@ CORS(app)
 MODEL_URL =  os.environ.get("MODEL_URL", default="http://localhost:8080/sentiment")
 MODEL_VERSION = os.environ.get("VERSION", default="latest")
 
+pageVisits = 0
+
 @app.route("/")
 def homepage():
+    global pageVisits
+    pageVisits += 1
     return render_template("index.html")
 
 @app.route("/model_url", methods=["GET"])
@@ -19,6 +23,14 @@ def get_model_url():
 @app.route("/model_version", methods=["GET"])
 def get_model_version():
     return jsonify({"model_version": MODEL_VERSION})
+
+@app.route("/metrics", methods=["GET"])
+def metrics():
+    global pageVisits
+    m = "# HELP pageVisits This shows the number of times this page was visited.\n"
+    m+= "# TYPE pageVisits counter\n"
+    m+= "num_requests{{page=\"\"}} {}\n".format(pageVisits)
+    return Response(m, mimetype="text/plain")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9999, debug=True)
