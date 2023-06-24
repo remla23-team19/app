858d2e7480781cafa47ede0a432bf41fca1d27d7
Philippe de Bekker <P.M.deBekker@student.tudelft.nl>
2023-06-24 15:36:31 +0200

    Adding documentation to all code

--- static/logic.js
+++ static/logic.js
@@ -1,3 +1,5 @@
+/* These lines of code are retrieving references to various HTML elements in the web page using their
+respective `id` attributes. */
 const queryForm = document.getElementById("query-form");
 const queryInput = document.getElementById("predict");
 const resultDiv = document.getElementById("result");
@@ -5,9 +7,17 @@ const correctButton = document.getElementById("correctButton");
 const wrongButton = document.getElementById("wrongButton");
 const savedParagraph = document.getElementById("saved");
 
+/* These lines of code are hiding the "Correct" and "Wrong" buttons in the HTML form by setting their
+CSS `display` property to "none". This is done initially when the page loads, so that the buttons
+are not visible until the user inputs some text and the sentiment analysis result is displayed. The
+buttons are then shown using the `showButtons()` function when the sentiment analysis result is
+displayed, and hidden again using the `disableButtons()` function after the user has provided
+feedback. */
 correctButton.style.display = "none";
 wrongButton.style.display = "none";
 
+/* These are event listeners that are added to the "Correct", "Wrong", and "Submit" buttons in the HTML
+form. */
 correctButton.addEventListener("click", async function () {
   disableButtons();
   saveSelection("correct");
@@ -26,6 +36,9 @@ queryForm.addEventListener("submit", function (event) {
   }
 });
 
+/**
+ * The function shows and enables two buttons regarding feedback of the result of the prompt sentiment.
+ */
 function showButtons() {
   correctButton.disabled = false;
   wrongButton.disabled = false;
@@ -33,6 +46,9 @@ function showButtons() {
   wrongButton.style.display = "inline-block";
 }
 
+/**
+ * The function disables and hides two buttons regarding feedback of the result of the prompt sentiment.
+ */
 function disableButtons() {
   correctButton.disabled = true;
   wrongButton.disabled = true;
@@ -40,6 +56,12 @@ function disableButtons() {
   wrongButton.style.display = "none";
 }
 
+/**
+ * This function saves a user's selection to a server and displays a "Saved!" message for 3 seconds.
+ * @param selection - The parameter `selection` is a variable that contains the user's selection or
+ * answer to a question. It is being passed as an argument to the `saveSelection` function. Should
+ * be either "correct" or "wrong".
+ */
 async function saveSelection(selection) {
   try {
     const response = await fetch("http://localhost/correctness", {
@@ -60,6 +82,10 @@ async function saveSelection(selection) {
   fadeOut(savedParagraph);
 }
 
+/**
+ * The function fades out an HTML element by gradually reducing its opacity.
+ * @param element - The HTML element that we want to fade out.
+ */
 function fadeOut(element) {
   let opacity = 1;
   const timer = setInterval(function () {
@@ -73,22 +99,43 @@ function fadeOut(element) {
   }, 30);
 }
 
+/**
+ * The function returns a promise that resolves after a specified amount of time has passed.
+ * @param ms - The parameter "ms" stands for milliseconds. It is the amount of time in milliseconds
+ * that the function will wait before resolving the promise.
+ * @returns The `sleep` function is returning a Promise object that resolves after a specified number
+ * of milliseconds.
+ */
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+/**
+ * The function retrieves the model URL from a server endpoint using asynchronous JavaScript.
+ * @returns The `getModelUrl` function is returning a Promise that resolves to the `model_url` value
+ * obtained from the JSON response of a fetch request to the "/model_url" endpoint.
+ */
 async function getModelUrl() {
   const response = await fetch("/model_url");
   const data = await response.json();
   return data.model_url;
 }
 
+/**
+ * This function retrieves the model version from a server using an asynchronous fetch request.
+ * @returns The `getModelVersion` function is returning the `model_version` property of the JSON data
+ * received from the `/model_version` endpoint after making a fetch request.
+ */
 async function getModelVersion() {
   const response = await fetch("/model_version");
   const data = await response.json();
   return data.model_version;
 }
 
+/* This code block is retrieving the model URL from a server endpoint using an asynchronous fetch
+request. It sets the `modelUrl` variable to the retrieved URL and updates the `href` and `innerHTML`
+properties of the `modelUrlLink` HTML element to display the URL. If there is an error retrieving
+the URL, it sets the `modelUrl` variable to a default URL and logs an error message to the console. */
 let modelUrl;
 const modelUrlLink = document.getElementById("modelUrlLink");
 getModelUrl()
@@ -105,6 +152,11 @@ getModelUrl()
     console.error(`Error getting model URL: ${error}`);
   });
 
+/* This code block is retrieving the model version from a server using an asynchronous fetch request.
+It sets the `modelVersion` variable to the retrieved version and updates the `innerHTML` property of
+the `modelVersionElement` HTML element to display the version. If there is an error retrieving the
+version, it sets the `modelVersion` variable to a default value of "latest" and logs an error
+message to the console. */
 let modelVersion;
 const modelVersionElement = document.getElementById("modelVersion");
 getModelVersion()
@@ -119,6 +171,12 @@ getModelVersion()
     console.error(`Error getting model version: ${error}`);
   });
 
+/**
+ * The function queries a model with input and displays the sentiment score and label in an HTML
+ * element.
+ * @param input - The input parameter is a string that represents the text to be analyzed for sentiment
+ * analysis. It is used to query a machine learning model and obtain a sentiment score.
+ */
 async function query(input) {
   console.log("Querying the model with input: " + input);
   const body = JSON.stringify({
@@ -160,10 +218,4 @@ async function query(input) {
   } catch (error) {
     resultDiv.innerHTML = "⚠️ </br> </br> <h6>Error: " + error + "</h6>";
   }
-}
-
-async function getModelUrl() {
-  const response = await fetch("/model_url");
-  const data = await response.json();
-  return data.model_url;
 }
